f8714653f2f2a56c7247ef7232e64a15c171d994
王洲权 <114213743+frank1467@users.noreply.github.com>
2024-07-17 13:31:35 +0800

    Add files via upload

---
+++ 3-homework/3-2-findThirdLargeNumber.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// 找出数组中第三大的数
+// input：一个数组array1，长度是10
+// output：数组中第三大的数thirdLargest
+// 例如：
+// 输入：
+// 1 2 3 4 5 6 7 8 9 10
+// 输出：
+// The third largest number is: 8
+
+int main(){
+    // input
+    int array1[10];
+    printf("Please enter 10 integers: \n");
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &array1[i]);
+    }
+    int thirdLargest = 0;
+
+    // Please add your code here
+
+
+
+
+    // output
+    printf("The third largest number is: %d\n", thirdLargest);
+
+    return 0;
+}
